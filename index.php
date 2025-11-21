e7556086bc6d010081d757de3d585c77f5806bd6
Elfoinquieto <cauanstocco@hotmail.com>
2025-11-20 23:30:56 -0300

    toma

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Koala WebStudio</title>
+    <link rel="stylesheet" href="css/informativas.css">
+    <link rel="stylesheet" href="css/admin-style.css">
+</head>
+<body>
+    <div >
+        <img class="logo" src="img/logo.jpeg" alt="Koala WebStudio" />
+    </div>
+    <div class="topo-direita">
+            <form action="logout.php" method="post" style="display:inline;">
+                <button type="submit" class="botao-sair">Sair</button>
+            </form>
+        </div>
+    <div class="main-content">
+        <div class="main-left">
+            <p class="koala">Koala WebStudio</p>
+            <h2>Criamos </h2>
+            <h2 class="mark">Web Sites</h2>
+            <h2>Criativos e Funcionais</h2>
+            <div class="divider"></div>
+            <p>
+                Desenvolvemos soluções digitais sob medida para conectar sua marca ao mundo, com sites modernos, rápidos e prontos para gerar resultados.
+            </p>
+            <div class="features-row">
+                <div class="feature">
+                    <h4>Inovação</h4>
+                    <p>Sites modernos, responsivos e otimizados para velocidade e usabilidade. Sua presença digital sempre atualizada com as melhores práticas do mercado.</p>
+                </div>
+                <div class="feature">
+                    <h4>Solução</h4>
+                    <p>Cada projeto é único: desenvolvemos serviços web personalizados que se adaptam às necessidades do seu negócio e ao perfil do seu público.</p>
+                </div>
+                <div class="feature">
+                    <h4>Confiança</h4>
+                    <p>Acompanhamos você em todas as etapas, oferecendo suporte dedicado e transparência total para que seu projeto cresça com segurança.</p>
+                </div>
+            </div>
+            <div class="buttons-row">
+                <button class="entrar"> <a href="#">Pacotes</a></button>
+                <button class="entrar"> <a href="#">Nossos Trabalhos</a></button>
+                <button class="entrar"> <a href="#">Sobre Nós</a></button>
+            </div>
+        </div>
+        <div class="main-right">
+            <img src="img/imagemHome.png" alt="Ilustração Web Dev" />
+        </div>
+    </div>
+</body>
+</html>
